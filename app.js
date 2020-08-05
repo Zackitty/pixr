cf925c05f679216972946fa6eedf6fc9c9dc19c3
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-08-05 17:25:20 -0500

    heroku deploy

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(function(err, _req, res, _next) {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/public'));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 }
--- client/server.js
+++ client/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
