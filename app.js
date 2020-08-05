93be88c3d6d6b8c312d23cfbaf58eb2c7c797297
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-08-05 15:51:40 -0500

    changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(helmet({ hsts: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(routes);
 
