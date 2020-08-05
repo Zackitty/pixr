f31d620924bcc287739cc1d3769c64a4723c978e
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-08-05 15:58:46 -0500

    changes

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ app.use(function(err, _req, res, _next) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
   app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 }
 
