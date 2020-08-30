8cae8b831fd0b4a42588abf6e738e40a7acc6575
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-08-30 00:37:18 -0500

    Heroku changes

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ app.use(function(err, _req, res, _next) {
 });
 
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+// }
 
 
 
