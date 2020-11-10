53fd96070dde0a865455529b5d99165c9c37494c
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-11-10 14:35:22 -0600

    Add to store

--- client/src/components/StreamBar.js
+++ client/src/components/StreamBar.js
@@ -2,9 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const StreamBar = (props) => {
+
+  const photoStreamClick = () => {}
+  const followStreamClick = () => {}
+  const albumsStreamClick = () => {}
+  const galleriesStreamClick = () => {}
+
   return (
-    <div>
-      
+    <div className="streamBarDiv">
+      <div classname='photoStreamButton' onClick={photoStreamClick}>Photostream</div>
+      <div classname='followsStreamButton' onClick={followStreamClick}>Followstream</div>
+      <div classname='albumsStreamButton' onClick={albumsStreamClick}>Albums</div>
+      <div classname='galleriesStreamButton' onClick={galleriesStreamClick}>Galleries</div>
     </div>
   )
 }
