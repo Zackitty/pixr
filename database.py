60f9d6ab2b9abf4f846ead201e10cabed6a73556
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-12-05 23:33:15 -0600

    add database

---
+++ database.py
@@ -0,0 +1,9 @@
+from app.models import User, Photo, Album
+from app import app, db
+from dotenv import load_dotenv
+import bcrypt
+load_dotenv()
+with app.app_context():
+  db.drop_all()
+  db.create_all()
+  
