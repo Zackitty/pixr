4e8d1b3c95c22e9de8eb89c228b2741d15b38ce1
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-12-14 20:55:26 -0600

    fix routes

--- app/api/routes/feeds.py
+++ app/api/routes/feeds.py
@@ -11,7 +11,7 @@ def index():
   response = Photo.query.all()
   return {photo.id: photo.to_dict() for photo in response}
 
-@bp.route('<int:id>/albums/<int:id>')
+@bp.route('<int:id>')
 
 @bp.route('<int:id>/albums/<int:id>')
 def index():
