443bf4946e63f5cd32f3043ff72ed14677d7e90c
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-12-26 22:12:13 -0600

    Add new route

--- app/api/routes/feeds.py
+++ app/api/routes/feeds.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, Photo
+from app.models import db, Photo, Album, albumPhotos
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 import bcrypt
 import re
@@ -11,7 +11,12 @@ def index():
   response = Photo.query.all()
   return {photo.id: photo.to_dict() for photo in response}
 
-@bp.route('<int:id>')
+@bp.route('/<int:id>')
+def user_photos(id):
+   response = Photo.query.filter(Photo.user_id == id)
+   return {photo.id: photo.to_dict() for photo in response}
 
-@bp.route('<int:id>/albums/<int:id>')
-def index():
+@bp.route('/albums/<int:id>')
+def albums_photos(id):
+    response = albumPhotos.query.filter(albumPhotos.Album.id == id)
+    return {photo.id: photo.to_dict() for photo in response}
